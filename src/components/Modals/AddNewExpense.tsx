5c6f03e448a80b08809108b45251b1a76a012109
Etem Senel <etemsenel@192.168.1.11>
2023-03-05 12:28:15 +0300

    datepicker added.

--- src/components/Modals/AddNewExpense.tsx
+++ src/components/Modals/AddNewExpense.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
 interface AddNewExpenseProps {
   setIsAddNewExpenseModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
   setIsCategoryModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
@@ -7,6 +11,18 @@ const AddNewExpense = ({
   setIsAddNewExpenseModalOpen,
   setIsCategoryModalOpen,
 }: AddNewExpenseProps) => {
+  const [newExpenseInputs, setNewExpenseInputs] = useState({
+    amount: 0,
+    category: "",
+    wallet: "",
+    date: new Date().toLocaleDateString() || new Date(),
+    note: "",
+  });
+
+  const [startDate, setStartDate] = useState(new Date());
+
+  console.log(newExpenseInputs);
+
   return (
     <div className="w-full ml-auto fixed min-h-screen top-0 bg-black bg-opacity-75 z-50">
       <div className="absolute bg-white rounded-mds shadow top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-11/12 max-w-3xl">
@@ -26,35 +42,55 @@ const AddNewExpense = ({
             <span className="text-xs pl-4 text-gray-500 tracking-wide">
               Category
             </span>
-            {/* {categories.map((category) => {
-              return (
-                <div id={category.name}>
-                  {category.name}
-                  <img src={category.image} />
-                </div>
-              );
-            })} */}
           </div>
-          <div className="col-span-1 h-16 border mx-4 rounded-md relative">
-            <span className="text-xs pl-4 text-gray-500 tracking-wide">
+          <div className="col-span-1 h-16 border mx-4 rounded-md relative flex hover:border-gray-500">
+            <label className="text-xs pl-4 pt-1 text-gray-500 tracking-wide w-full h-full">
               Amount
-            </span>
-            <input
-              className="absolute left-0 w-full h-full"
-              type="number"
-              name=""
-              id=""
-            />
+              <input
+                onChange={(e) =>
+                  setNewExpenseInputs({
+                    ...newExpenseInputs,
+                    amount: parseInt(e.target.value),
+                  })
+                }
+                value={newExpenseInputs.amount}
+                className="w-full h-full absolute left-0 bottom-0 -z-20 text-base text-black pt-4 px-4 focus:outline-none"
+                type="number"
+              />
+            </label>
           </div>
-          <div className="col-span-1 h-16 border mx-4 rounded-md">
-            <span className="text-xs pl-4 text-gray-500 tracking-wide">
+          <div className="col-span-1 h-16 border mx-4 rounded-md relative flex hover:border-gray-500">
+            <label className="text-xs pl-4 pt-1 text-gray-500 tracking-wide w-full h-full">
               Date
-            </span>
+              <DatePicker
+                selected={startDate}
+                onChange={(date) => {
+                  setStartDate(date ? date : new Date());
+                  setNewExpenseInputs({
+                    ...newExpenseInputs,
+                    date: date ? date.toLocaleDateString() : new Date(),
+                  });
+                }}
+                className="w-full h-full mt-3 focus:outline-none"
+                dateFormat="MM/dd/yyyy"
+              />
+            </label>
           </div>
-          <div className="col-span-2 h-16 border mx-4 rounded-md">
-            <span className="text-xs pl-4 text-gray-500 tracking-wide">
+
+          <div className="col-span-2 h-16 border mx-4 rounded-md relative flex hover:border-gray-500">
+            <label className="text-xs pl-4 pt-1 text-gray-500 tracking-wide w-full h-full">
               Note
-            </span>
+              <input
+                onChange={(e) =>
+                  setNewExpenseInputs({
+                    ...newExpenseInputs,
+                    note: e.target.value,
+                  })
+                }
+                className="w-full h-full absolute left-0 bottom-0 -z-20 text-base text-black pt-4 px-4 focus:outline-none"
+                type="text"
+              />
+            </label>
           </div>
         </div>
         <div className="w-11/12 my-6 flex justify-end mx-auto">
@@ -64,7 +100,10 @@ const AddNewExpense = ({
           >
             Cancel
           </button>
-          <button className="w-28 h-12 bg-gray-100 uppercase text-sm font-semibold mx-4">
+          <button
+            onClick={() => console.log(newExpenseInputs)}
+            className="w-28 h-12 bg-gray-100 uppercase text-sm font-semibold mx-4"
+          >
             Save
           </button>
         </div>
